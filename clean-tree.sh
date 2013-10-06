35317be423943f13cd102d62403ebd8a70c96eca
Matthew Brush <mbrush@codebrainz.ca>
2013-10-06 00:00:16 -0700

    Add script for cleaning up Autotools cruft

---
+++ clean-tree.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Deletes a bunch of junk put in the tree by Auto-tools
+
+rm -f *.o *.a *.so *.dll *.lib *.dylib *.lo *.la
+rm -rf .deps/ .libs/ autom4te.cache/ build-aux/
+rm -f m4/libtool.m4 m4/lt*.m4 Makefile Makefile.in libtool
+rm -f config.* configure stamp-h1 aclocal.m4
