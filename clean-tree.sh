d1fe7c2971d37debbfb331c25b8850ab7096396d
Matthew Brush <mbrush@codebrainz.ca>
2013-10-06 05:07:27 -0700

    Fixup paths in clean-tree.sh script

--- clean-tree.sh
+++ clean-tree.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # Deletes a bunch of junk put in the tree by Auto-tools
 
-rm -f *.o *.a *.so *.dll *.lib *.dylib *.lo *.la
-rm -rf .deps/ .libs/ autom4te.cache/ build-aux/
-rm -f m4/libtool.m4 m4/lt*.m4 Makefile Makefile.in libtool
-rm -f config.* configure stamp-h1 aclocal.m4
+rm -f *.o *.a *.so *.dll *.lib *.dylib *.lo *.la config.*
+rm -rf .deps/ .libs/ autom4te.cache/ build-aux/ m4/
+rm -f configure stamp-h1 aclocal.m4 libtool Makefile Makefile.in
 rm -f compile_commands.json
