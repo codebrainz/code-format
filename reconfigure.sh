b03d72f6fa3b8b39067ca4da9fb0ee2621979c8e
Matthew Brush <mbrush@codebrainz.ca>
2013-10-06 03:26:56 -0700

    Add compile_commands.json for some Clang tools to use
    Initial attempt, untested, and requires ./configure is run with
    the clang path wanted in the .json file. Need to review the paths
    WRT source vs build dirs.

--- reconfigure.sh
+++ reconfigure.sh
@@ -4,9 +4,11 @@
 # to where Geany GTK3 is installed.
 
 PREFIX=$HOME/.local
+LLVM_PREFIX=/opt/llvm
 
 make clean
 make distclean
 ./autogen.sh && \
   PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig" \
+  CC="${LLVM_PREFIX}/bin/clang" \
     ./configure --prefix="${PREFIX}"
